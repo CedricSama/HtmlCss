d5d1c7ce87b3d0f8b7078c3795de059ce02d76f5
Cedric <sajlksdfkg@msdlmglmfg/fr>
2017-10-17 16:43:52 +0200

    commit 11.7

---
+++ Marlene/TPMarlene/js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+
+
+/*
+$('.carousel.carousel-slider').carousel({fullWidth: true});*/
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
+});
